2985b282710c6b2d092af6a1d982c1f9f4063a22
Prim Asghar <mirpasghar@gmail.com>
2022-03-16 13:58:00 +0200

    Added Search Input and some styleing

--- src/shared/Header/Header.js
+++ src/shared/Header/Header.js
@@ -1,7 +1,14 @@
 import React from "react";
 
+import classes from "./Header.module.css";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <form className={classes.movieSearch}>
+      <input type="search" id="search" placeholder="movie name"></input>
+      <button className={classes.btn}>Search</button>
+    </form>
+  );
 };
 
 export default Header;
