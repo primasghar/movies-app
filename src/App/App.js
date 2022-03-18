422015ab2690d43f97f64d1fd5d160e522e1893e
Prim Asghar <mirpasghar@gmail.com>
2022-03-18 22:48:37 +0200

    Added main class to all route and edited css

--- src/App/App.js
+++ src/App/App.js
@@ -8,28 +8,37 @@ import ComingSoon from "../ComingSoon";
 import PopularMovies from "../PopularMovies";
 import PopularPeople from "../PopularPeople";
 import TopRated from "../TopRated";
+import MovieDetail from "../MovieDetail";
+import NotFound from "../shared/NotFound/NotFound";
 
 import classes from "./App.module.css";
 
 const App = () => {
   return (
-    <div className={classes.App}>
+    <div className={classes.app}>
       <section className={classes.sec_1}>
         <SideNav />
       </section>
       <section className={classes.sec_2}>
         <Header className={classes.search} />
 
-        <Routes>
-          <Route path="/" element={<NewRelease />} />
-          <Route path="comingsoon" element={<ComingSoon />} />
-          <Route path="popularmovies" element={<PopularMovies />} />
-          <Route path="popularpeople" element={<PopularPeople />} />
-          <Route path="toprated" element={<TopRated />} />
-        </Routes>
+        <div className={classes.main}>
+          <Routes>
+            <Route path="/" element={<NewRelease />} />
+            <Route path="/comingsoon" element={<ComingSoon />} />
+            <Route path="/popularmovies" element={<PopularMovies />} />
+            <Route path="/popularpeople" element={<PopularPeople />} />
+            <Route path="/toprated" element={<TopRated />} />
+            <Route path="/moviedetail/:id" element={<MovieDetail />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
       </section>
     </div>
   );
 };
 
 export default App;
+
+// Dynamic Routes with Params
+//* Route is considered when no other Route matches
