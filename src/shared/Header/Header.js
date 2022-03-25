28b97f928cc278a716c5f37344b1a0de4ccf546f
Prim Asghar <mirpasghar@gmail.com>
2022-03-25 21:15:04 +0200

    added title and removed form and placeholder

--- src/shared/Header/Header.js
+++ src/shared/Header/Header.js
@@ -1,19 +1,22 @@
 import React from "react";
 
+import Button from "../Button";
 import classes from "./Header.module.css";
 
 const Header = () => {
   return (
-    <div className={classes.movieSearch}>
-      <form>
-        <button className={classes.btn}>Search</button>
+    <div className={classes.header}>
+      <h1 className={classes.logo}>Show Time</h1>
+
+      <div className={classes.movieSearch}>
+        <Button>Search</Button>
         <input
           type="search"
           id="search"
-          placeholder="movie name"
+          // placeholder="movie name"
           className={classes.searchBox}
         />
-      </form>
+      </div>
     </div>
   );
 };
