c14c0381c1003ea47f297eeb8c4de65fe232f34f
Prim Asghar <mirpasghar@gmail.com>
2022-03-31 20:27:20 +0300

    Changed width and height of poster and div to section

--- src/MovieDetail/MovieDetail.js
+++ src/MovieDetail/MovieDetail.js
@@ -20,10 +20,10 @@ const MovieDetail = () => {
           className={classes.img}
           alt="movie poster"
           src={test}
-          width={200}
-          height={250}
+          width={300}
+          height={450}
         />
-        <div>
+        <section>
           <h1>Enchanto</h1>
           <span>2021</span>
           <br />
@@ -37,7 +37,7 @@ const MovieDetail = () => {
           <span>Director and Screenplay</span>
           <br />
           {/* <p>{params.id}</p> */}
-        </div>
+        </section>
       </div>
       <div>
         <span>Cast</span>
@@ -53,3 +53,29 @@ export default MovieDetail;
 //Use dynamic Route in App to set path with dynamic id and then setting id here to use to display data
 //dynamically when certain id movie is clicked and show the details
 //Can use Redux for that to send http requests.
+
+// {
+
+//   "id": 7555,
+//   "title": "Rambo",
+//   "release_date": "2008-01-24",
+//   "original_language": "en",
+//   "adult": false,
+//   "backdrop_path": "/wZVluw8j7i7TPsrrh7yhSE0ekCc.jpg",
+//   "poster_path": "/45YC86aMjWlcPkvgWwqdI6Clq7b.jpg",
+//   "genres": [
+//       {
+//           "id": 28,
+//           "name": "Action"
+//       },
+//       {
+//           "id": 53,
+//           "name": "Thriller"
+//       }
+//   ],
+//   "runtime": 92,
+//   "vote_average": 6.6,
+//   "tagline": "Heroes never die... They just reload.",
+//   "overview": "When governments fail to act on behalf of captive missionaries, ex-Green Beret John James Rambo sets aside his peaceful existence along the Salween River in a war-torn region of Thailand to take action.  Although he's still haunted by violent memories of his time as a U.S. soldier during the Vietnam War, Rambo can hardly turn his back on the aid workers who so desperately need his help.",
+
+// // }
