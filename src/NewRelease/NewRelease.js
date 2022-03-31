ace68d02cc91f448aa15765e58fbfc62bccd9514
Prim Asghar <mirpasghar@gmail.com>
2022-03-31 20:36:31 +0300

    Used axios to fetch movie data and react pagination to pages. Added styles to the pagination

--- src/NewRelease/NewRelease.js
+++ src/NewRelease/NewRelease.js
@@ -1,10 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ReactPaginate from "react-paginate";
+import axios from "axios";
 
 import MovieList from "../shared/MovieList/MovieList";
-//import classes from "./NewRelease.module.css";
+import classes from "./NewRelease.module.css";
 
 const NewRelease = () => {
-  return <MovieList />;
+  const [data, setData] = useState(null);
+  const [currentPageNumber, setCurrentPageNumber] = useState(1);
+
+  const URL = `https://api.themoviedb.org/3/movie/now_playing?api_key=44215a69b2337d878932ea0a9d2088d4&language=en-US&page=${currentPageNumber}`;
+
+  useEffect(() => {
+    axios.get(URL).then((response) => setData(response.data));
+  }, [URL, currentPageNumber]);
+
+  const getNextPage = (event) => {
+    setCurrentPageNumber(event.selected + 1);
+  };
+
+  if (!data) {
+    return "Loading...";
+  }
+
+  return (
+    <div>
+      <MovieList movies={data.results} />
+
+      <ReactPaginate
+        nextLabel={">"}
+        onPageChange={getNextPage}
+        pageRangeDisplayed={5}
+        pageCount={data.total_pages}
+        previousLabel={"<"}
+        renderOnZeroPageCount={null}
+        className={classes.paginationBttns}
+        previousClassName={classes.previousBttn}
+        nextClassName={classes.nextBttn}
+        activeClassName={classes.paginationActive}
+        disabledClassName={classes.paginationDisabled}
+      />
+    </div>
+  );
 };
 
 export default NewRelease;
