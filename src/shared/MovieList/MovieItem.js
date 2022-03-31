094ed26e013c82c6a9d669a8cfc80a3f5f27c49f
Prim Asghar <mirpasghar@gmail.com>
2022-03-31 20:32:07 +0300

    Changed width & height of poster and added star image, release date and made Link of the title. Add styles

--- src/shared/MovieList/MovieItem.js
+++ src/shared/MovieList/MovieItem.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import test from "../../assets/images/test.jpg";
-
+import star from "../../assets/images/star.png";
 import classes from "./MovieItem.module.css";
 
 const MovieItem = ({ movieData, handledetail }) => {
@@ -10,17 +9,24 @@ const MovieItem = ({ movieData, handledetail }) => {
     <div className={classes.movieItem}>
       <Link to={`/moviedetail/${movieData.id}`}>
         <img
-          src={test}
+          src={`https://image.tmdb.org/t/p/w500/${movieData.poster_path}`}
           alt="poster"
-          width={200}
-          height={250}
+          width={234}
+          height={330}
           onClick={handledetail}
+          className={classes.poster}
         />
       </Link>
 
-      <div className={classes.genre}>
-        <span className={classes.title}>{"Enchanto"}</span>
-        <span className={classes.rating}>{movieData.rating}</span>
+      <div className={classes.movieInfo}>
+        <div>
+          <img src={star} alt="rating star" className={classes.star} />
+          <span className={classes.rating}>{movieData.vote_average}</span>
+        </div>
+        <Link to={`/moviedetail/${movieData.id}`} className={classes.title}>
+          {movieData.title}
+        </Link>
+        <p className={classes.releaseDate}>{movieData.release_date}</p>
       </div>
     </div>
   );
