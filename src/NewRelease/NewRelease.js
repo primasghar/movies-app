750db8079fd9461f5c97c39dd8eaec96f27369d2
Prim Asghar <mirpasghar@gmail.com>
2022-04-02 00:15:38 +0300

    Added code to fetch new movies using Redux store, Notification and Pagination

--- src/NewRelease/NewRelease.js
+++ src/NewRelease/NewRelease.js
@@ -1,37 +1,54 @@
 import React, { useEffect, useState } from "react";
-import ReactPaginate from "react-paginate";
-import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
 
 import MovieList from "../shared/MovieList/MovieList";
+import ReactPaginate from "react-paginate";
+import Notification from "../shared/Notification";
+import { fetchMovieData } from "../store/movie-action";
 import classes from "./NewRelease.module.css";
 
 const NewRelease = () => {
-  const [data, setData] = useState(null);
+  const dispatch = useDispatch();
+  const movies = useSelector((state) => state.movie.moviesList);
+
+  const notification = useSelector((state) => state.ui.notification);
+
   const [currentPageNumber, setCurrentPageNumber] = useState(1);
 
   const URL = `https://api.themoviedb.org/3/movie/now_playing?api_key=44215a69b2337d878932ea0a9d2088d4&language=en-US&page=${currentPageNumber}`;
 
   useEffect(() => {
-    axios.get(URL).then((response) => setData(response.data));
-  }, [URL, currentPageNumber]);
+    dispatch(fetchMovieData(URL));
+  }, [URL, currentPageNumber, dispatch]);
 
   const getNextPage = (event) => {
     setCurrentPageNumber(event.selected + 1);
   };
 
-  if (!data) {
-    return "Loading...";
+  console.log("NewRelease", notification);
+
+  if (notification.status === "loading" || notification.status === "pending") {
+    return "loading";
   }
 
   return (
     <div>
-      <MovieList movies={data.results} />
+      {notification.status === "error" && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <MovieList movies={movies.results} />
+
+      {/* <CommonPagination fecthNext= {() => fetchMovieData(URL)} /> */}
 
       <ReactPaginate
         nextLabel={">"}
         onPageChange={getNextPage}
         pageRangeDisplayed={5}
-        pageCount={data.total_pages}
+        pageCount={movies.total_pages}
         previousLabel={"<"}
         renderOnZeroPageCount={null}
         className={classes.paginationBttns}
