bed7da27239c8428e312143a46235b8f6716800e
Prim Asghar <mirpasghar@gmail.com>
2022-04-02 00:27:17 +0300

    Provided Redux store to the App component to make it available to all components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import store from "./store/store";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
@@ -7,7 +9,9 @@ import App from "./App/App";
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById("root")
 );
