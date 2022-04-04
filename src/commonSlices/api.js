d85a4eb5c65a8f7e5b03c3cde654bb3c0ffd1e14
Prim Asghar <mirpasghar@gmail.com>
2022-04-04 21:47:05 +0300

    Used template literal to build dynamic URL string and posted all URL need for app in commented form

--- src/commonSlices/api.js
+++ src/commonSlices/api.js
@@ -1,8 +1,26 @@
 import axios from "utils/axios";
 
-const getMovies = (moviesType, currentPageNumber) =>
-  axios.get(
-    `${moviesType}?api_key=44215a69b2337d878932ea0a9d2088d4&language=en-US&page=${currentPageNumber}`
-  );
+const getMovies = (moviesType, currentPageNumber) => {
+  const baseURL = "https://api.themoviedb.org/3/movie/";
+  const queryParam =
+    "?api_key=44215a69b2337d878932ea0a9d2088d4&language=en-US&page=";
+
+  axios.get(`${baseURL}${moviesType}${queryParam}${currentPageNumber}`);
+};
 
 export default getMovies;
+
+// NewRelease=now_playing
+// https://api.themoviedb.org/3/movie/now_playing?api_key=44215a69b2337d878932ea0a9d2088d4&language=en-US&page=1;
+
+// PopularMovies=popular
+// https://api.themoviedb.org/3/movie/popular?api_key=44215a69b2337d878932ea0a9d2088d4language=en-US&page=1
+
+// ComingSoon=upcoming
+//https://api.themoviedb.org/3/movie/upcoming?api_key=44215a69b2337d878932ea0a9d2088d4&language=en-US&page=1
+
+// TopRated=top_rated
+// https://api.themoviedb.org/3/movie/top_rated?api_key=44215a69b2337d878932ea0a9d2088d4&language=en-US&page=1
+
+//PopularPeople
+// https://api.themoviedb.org/3/person/1136406/images?api_key=44215a69b2337d878932ea0a9d2088d4
