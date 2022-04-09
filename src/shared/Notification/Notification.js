bdf213e11be5af9a9b437a8be8edd0def3186a98
Prim Asghar <mirpasghar@gmail.com>
2022-04-09 16:44:53 +0300

    Added css class for success status and removed p element for message

--- src/shared/Notification/Notification.js
+++ src/shared/Notification/Notification.js
@@ -3,14 +3,15 @@ import classes from "./Notification.module.css";
 const Notification = (props) => {
   let specialClasses = "";
 
+  if (props.status === "loading") {
+    specialClasses = classes.loading;
+  }
+
   if (props.status === "error") {
     specialClasses = classes.error;
   }
-  // if (props.status === "success") {
-  //   specialClasses = classes.success;
-  // }
-  if (props.status === "loading") {
-    specialClasses = classes.loading;
+  if (props.status === "success") {
+    specialClasses = classes.success;
   }
 
   const cssClasses = `${classes.notification} ${specialClasses}`;
@@ -18,7 +19,6 @@ const Notification = (props) => {
   return (
     <section className={cssClasses}>
       <h2>{props.title}</h2>
-      <p>{props.message}</p>
     </section>
   );
 };
