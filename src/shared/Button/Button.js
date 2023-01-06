19af736cda0ab83ce2b6b2340c8a746a38bafac2
Prim Asghar <mirpasghar@gmail.com>
2023-01-07 01:38:21 +0200

    Error corrected by replacing div with button div

--- src/shared/Button/Button.js
+++ src/shared/Button/Button.js
@@ -4,9 +4,9 @@ import classes from "./Button.module.css";
 
 const Button = (props) => {
   return (
-    <div className={classes.btn} type={props.type} onClick={props.onClick}>
+    <button className={classes.btn} type={props.type} onClick={props.onClick}>
       {props.children}
-    </div>
+    </button>
   );
 };
 
