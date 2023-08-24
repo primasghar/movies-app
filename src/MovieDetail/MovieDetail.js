1f742acd4a864e752d865b23081b7c3566efe4c6
Prim Asghar <mirpasghar@gmail.com>
2023-08-24 12:57:13 +0300

    Added new classes and styling in Movie Detail Component.

--- src/MovieDetail/MovieDetail.js
+++ src/MovieDetail/MovieDetail.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 
 import classes from "./MovieDetail.module.css";
 
@@ -15,67 +15,51 @@ const MovieDetail = () => {
     axios.get(URL).then((response) => setData(response.data));
   }, [URL]);
 
-  console.log("data", data);
   if (!data) {
     return <p>Loading</p>;
   }
+
+  const renderGenres = data.genres.map((genre, index) => {
+    if (index === data.genres.length - 1) {
+      return <li className={classes.genreItems}>{genre.name}</li>;
+    }
+
+    return <li className={classes.genreItems}>{genre.name}, </li>;
+  });
+
   return (
-    <main className={classes.wrapper}>
-      <div className={classes.detail}>
-        <img
-          className={classes.img2}
-          alt="movie poster"
-          src={`https://image.tmdb.org/t/p/w500${data.poster_path}`}
-          width={500}
-          height={500}
-        />
-        <section>
+    <main className={classes.container}>
+      <div className={classes.topDetail}>
+        <section className={classes.imgSection}>
+          <img
+            className={classes.img2}
+            alt="movie poster"
+            src={`https://image.tmdb.org/t/p/w500${data.poster_path}`}
+            // width={500}
+            height={500}
+          />
+        </section>
+        <section className={classes.movieInfo}>
           <h1>{data.title}</h1>
-            <div>
-          <h3>Release Date: {data.release_date}</h3>
-          <h3>Genres:
-            {data.genres.map((genre) => (
-              <span>{genre.name}</span>
-            ))}
-          </h3>
+          <ul className={classes.infoList}>
+            <li className={classes.infoItem}>{data.release_date}</li>
+            <li className={classes.infoItem}> - </li>
+            <li className={classes.infoItem}>
+              <ul className={classes.genreList}>{renderGenres}</ul>
+            </li>
+            <li className={classes.infoItem}> - </li>
+            <li className={classes.infoItem}>{data.runtime}</li>
+          </ul>
+
+          <h3>{data.tagline}</h3>
           <h3>Overview</h3>
           <p>{data.overview}</p>
-            </div>
         </section>
       </div>
+
+      <section></section>
     </main>
   );
 };
 
 export default MovieDetail;
-
-//  useParams hook to extract route params
-//Use dynamic Route in App to set path with dynamic id and then setting id here to use to display data
-//dynamically when certain id movie is clicked and show the details
-//Can use Redux for that to send http requests.
-
-// {
-
-//   "id": 7555,
-//   "title": "Rambo",
-//   "release_date": "2008-01-24",
-//   "original_language": "en",
-//   "adult": false,
-//   "backdrop_path": "/wZVluw8j7i7TPsrrh7yhSE0ekCc.jpg",
-//   "poster_path": "/45YC86aMjWlcPkvgWwqdI6Clq7b.jpg",
-//   "genres": [
-//       {
-//           "id": 28,
-//           "name": "Action"
-//       },
-//       {
-//           "id": 53,
-//           "name": "Thriller"
-//       }
-//   ],
-//   "runtime": 92,
-//   "vote_average": 6.6,
-//   "tagline": "Heroes never die... They just reload.",
-//   "overview": "When governments fail to act on behalf of captive missionaries, ex-Green Beret John James Rambo sets aside his peaceful existence along the Salween River in a war-torn region of Thailand to take action.  Although he's still haunted by violent memories of his time as a U.S. soldier during the Vietnam War, Rambo can hardly turn his back on the aid workers who so desperately need his help.",
-
-// // }
