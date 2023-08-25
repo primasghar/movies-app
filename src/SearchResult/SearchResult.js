052bed6cb40d7b6bff963b370f495fd06946cc5b
Prim Asghar <mirpasghar@gmail.com>
2023-08-25 22:41:19 +0300

    Added Search functionality

---
+++ src/SearchResult/SearchResult.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import MovieList from "../shared/MovieList";
+import Paginate from "../shared/Paginate";
+
+import classes from "./SearchResult.module.css";
+
+const SearchResult = () => {
+  const [data, setData] = useState(null);
+  const [currentPageNumber, setCurrentPageNumber] = useState(1);
+
+  let { query } = useParams();
+
+  const URL = `https://api.themoviedb.org/3/search/movie?api_key=44215a69b2337d878932ea0a9d2088d4&language=en-US&query=${query}&page=${currentPageNumber}`;
+
+  useEffect(() => {
+    axios.get(URL).then((response) => setData(response.data));
+  }, [URL, currentPageNumber]);
+
+  const getNextPage = (event) => {
+    setCurrentPageNumber(event.selected + 1);
+  };
+
+  if (!data) {
+    return <p>Loading</p>;
+  }
+
+  if (!data.results.length) {
+    return <p>No results found</p>;
+  }
+
+  return (
+    <div>
+      <MovieList movies={data.results} />
+
+      <Paginate
+        nextLabel={">"}
+        onPageChange={getNextPage}
+        pageRangeDisplayed={5}
+        pageCount={data.total_pages}
+        previousLabel={"<"}
+      />
+    </div>
+  );
+};
+
+export default SearchResult;
