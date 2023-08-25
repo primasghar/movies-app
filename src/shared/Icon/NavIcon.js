f2c93c11707578ded84f106586b26cc348f67f78
Prim Asghar <mirpasghar@gmail.com>
2023-08-25 22:35:55 +0300

    New Navigation icon added

---
+++ src/shared/Icon/NavIcon.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Icon } from "react-icons-kit";
+import { navicon } from "react-icons-kit/fa/navicon";
+
+const NavIcon = () => {
+  return (
+    <div style={{ color: "#edf2fb" }}>
+      <Icon icon={navicon} size={26} />
+    </div>
+  );
+};
+
+export default NavIcon;
