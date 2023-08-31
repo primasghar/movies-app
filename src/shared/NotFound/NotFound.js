f5a5b302a466d5c4d6047035257abb1961f3f9c9
Prim Asghar <mirpasghar@gmail.com>
2023-08-31 23:39:20 +0300

    Added Link and change the paragraph.

--- src/shared/NotFound/NotFound.js
+++ src/shared/NotFound/NotFound.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import classes from "./NotFound.module.css";
 
 const NotFound = () => {
   return (
-    <div>
-      <p>Page Not Found</p>
+    <div className={classes.notFound}>
+      <p className={classes.noPage}>Sorry! Page Not Found</p>
+      <p className={classes.linkToHome}>
+        Go to the <Link to="/">Home Page</Link>
+      </p>
     </div>
   );
 };
